ab62fc6699cb25c600250de7b756fd59c29cf954
Simha_girish <simhagirishbotta@gmail.com>
2026-06-20 09:24:04 +0530

    add env db

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +91,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
